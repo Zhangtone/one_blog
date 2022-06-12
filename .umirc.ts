2e7f4c774263cf25f1c388504282a14430f797ed
JinBiao <1461471243@qq.com>
2022-06-12 10:54:47 +0800

    首次部署

---
+++ .umirc.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'dumi';
+const repo = 'one_blog'; // 项目名
+export default defineConfig({
+  title: repo,
+  mode: 'site',
+  devServer: {
+    port: 1998, // 自定义端口号
+  },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  // more config: https://d.umijs.org/config
+});
